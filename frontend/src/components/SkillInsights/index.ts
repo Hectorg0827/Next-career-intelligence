ecc331581a5cae9afc304505091e82867561dc1e
Hectorg0827 <hector.garcia0827@gmail.com>
2025-10-19 16:09:05 -0400

    Create index.ts

---
+++ frontend/src/components/SkillInsights/index.ts
@@ -0,0 +1,6 @@
+export { default as SkillInsightsPanel } from './SkillInsightsPanel';
+export { default as SkillStrengthMeter } from './SkillStrengthMeter';
+export { default as SkillClustersCard } from './SkillClustersCard';
+export { default as TransferableSkillsCard } from './TransferableSkillsCard';
+export { default as HiddenSkillsBadge } from './HiddenSkillsBadge';
+export { default as SkillGapsRoadmap } from './SkillGapsRoadmap';
