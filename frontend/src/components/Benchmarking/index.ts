b3b0c930599647b0446be00e78138b2f1df1fbbe
Hectorg0827 <hector.garcia0827@gmail.com>
2025-10-19 16:08:26 -0400

    Create index.ts

---
+++ frontend/src/components/Benchmarking/index.ts
@@ -0,0 +1,4 @@
+export { default as RiskComparisonBadge } from './RiskComparisonBadge';
+export { default as BenchmarkChart } from './BenchmarkChart';
+export { default as ProgressTracker } from './ProgressTracker';
+export { default as TrendIndicator } from './TrendIndicator';
