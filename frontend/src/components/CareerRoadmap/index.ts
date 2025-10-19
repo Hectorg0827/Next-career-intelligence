e500137e5bc53637a50fc24e26be642377451558
Hectorg0827 <hector.garcia0827@gmail.com>
2025-10-19 16:08:50 -0400

    Create index.ts

---
+++ frontend/src/components/CareerRoadmap/index.ts
@@ -0,0 +1,5 @@
+export { default as CareerRoadmapTimeline } from './CareerRoadmapTimeline';
+export { default as PathwayCard } from './PathwayCard';
+export { default as ImmediateStepsCard } from './ImmediateStepsCard';
+export { default as RiskMitigationCard } from './RiskMitigationCard';
+export { default as PathwayVisualization } from './PathwayVisualization';
